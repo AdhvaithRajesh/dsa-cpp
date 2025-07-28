fe70733f874db9176b612f3449a02fc1fc7425fa
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2025-07-28 14:01:21 +0530

    add unordered_multiset

---
+++ basics/cppstl/multiset.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+/*
+hash-table based container that stores elements in an unordered fashion
+but allows duplicate elements
+fast operations - just like unordered set - O(1)
+
+When is an unordered multiset used?
+- less common than vectors, sets, unordered_sets
+- frequency counting (as an alternative to map) - instead of mapping element to count, store elements directly
+    - this is a simpler solution. unordered_maps are more powerful since we can associate data with count, easily iterate through unique keys
+- anagram checking
+    - add elements of s1 into an unordered_multiset
+    - iterate through elements of s2 and remove from s1
+        - if .find() returns .end() - not an anagram
+        - if loop is complete and set is empty - anagram
+- bag semantics
+    - any problem where intersection or difference needs to be found while respecting duplicates
+    - ex. A = {1, 2, 2}, B = {2, 2, 3}
+    - intersection = {2, 2} - this can easily be found using unordered_multiset
+*/
+
+template <typename T>
+void print_multiset(std::unordered_multiset<T> &mset)
+{
+    for (const auto &element : mset)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    // declaration and initialization
+    std::unordered_multiset<int> int_mset = {10, 20, 10, 30, 10, 20, 30};
+
+    print_multiset(int_mset);
+
+    std::cout << "Total size of this unordered multiset is: " << int_mset.size() << "\n\n";
+
+    /*
+    core operations
+    insert() - similar to the one with unordered set, but adds element even if it already present
+    count() - more powerful than regular set - returns total number of occurences of that element
+    erase() - IMPORTANT
+            - erase(value) - erases all occurences of that value
+            - erase(iterator) - erases only the single element pointed to by that iterator
+    */
+
+    std::cout << "Set before int_mset.erase(10):\n";
+    print_multiset(int_mset);
+    std::cout << "Number of 10s before int_mset.erase(10): ";
+    std::cout << int_mset.count(10) << "\n\n";
+
+    int_mset.erase(10);
+    std::cout << "Set after int_mset.erase(10):\n";
+    print_multiset(int_mset);
+    std::cout << "Number of 10s after int_mset.erase(10): ";
+    std::cout << int_mset.count(10) << "\n\n";
+
+    std::cout << "Using an iterator to erase an element - 20\n";
+    std::cout << "Number of 20s before int_mset.erase(iterator): ";
+    std::cout << int_mset.count(20) << "\n\n";
+    auto it = int_mset.find(20); // returns iterator to the first occurence of the argument
+    if (it != int_mset.end())
+    {
+        int_mset.erase(it);
+    }
+    print_multiset(int_mset);
+    std::cout << "Number of 20s after int_mset.erase(iterator): ";
+    std::cout << int_mset.count(20);
+    return 0;
+}
