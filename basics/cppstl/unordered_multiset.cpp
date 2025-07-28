8c7b70dbbb8ce6373f94cbe4e2ad1d6ebb46f8a9
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2025-07-28 14:09:22 +0530

    modify int_mset to int_umset in unordered_multiset.cpp

--- basics/cppstl/unordered_multiset.cpp
+++ basics/cppstl/unordered_multiset.cpp
@@ -35,11 +35,11 @@ void print_multiset(std::unordered_multiset<T> &mset)
 int main()
 {
     // declaration and initialization
-    std::unordered_multiset<int> int_mset = {10, 20, 10, 30, 10, 20, 30};
+    std::unordered_multiset<int> int_umset = {10, 20, 10, 30, 10, 20, 30};
 
-    print_multiset(int_mset);
+    print_multiset(int_umset);
 
-    std::cout << "Total size of this unordered multiset is: " << int_mset.size() << "\n\n";
+    std::cout << "Total size of this unordered multiset is: " << int_umset.size() << "\n\n";
 
     /*
     core operations
@@ -50,27 +50,27 @@ int main()
             - erase(iterator) - erases only the single element pointed to by that iterator
     */
 
-    std::cout << "Set before int_mset.erase(10):\n";
-    print_multiset(int_mset);
-    std::cout << "Number of 10s before int_mset.erase(10): ";
-    std::cout << int_mset.count(10) << "\n\n";
+    std::cout << "Set before int_umset.erase(10):\n";
+    print_multiset(int_umset);
+    std::cout << "Number of 10s before int_umset.erase(10): ";
+    std::cout << int_umset.count(10) << "\n\n";
 
-    int_mset.erase(10);
-    std::cout << "Set after int_mset.erase(10):\n";
-    print_multiset(int_mset);
-    std::cout << "Number of 10s after int_mset.erase(10): ";
-    std::cout << int_mset.count(10) << "\n\n";
+    int_umset.erase(10);
+    std::cout << "Set after int_umset.erase(10):\n";
+    print_multiset(int_umset);
+    std::cout << "Number of 10s after int_umset.erase(10): ";
+    std::cout << int_umset.count(10) << "\n\n";
 
     std::cout << "Using an iterator to erase an element - 20\n";
-    std::cout << "Number of 20s before int_mset.erase(iterator): ";
-    std::cout << int_mset.count(20) << "\n\n";
-    auto it = int_mset.find(20); // returns iterator to the first occurence of the argument
-    if (it != int_mset.end())
+    std::cout << "Number of 20s before int_umset.erase(iterator): ";
+    std::cout << int_umset.count(20) << "\n\n";
+    auto it = int_umset.find(20); // returns iterator to the first occurence of the argument
+    if (it != int_umset.end())
     {
-        int_mset.erase(it);
+        int_umset.erase(it);
     }
-    print_multiset(int_mset);
-    std::cout << "Number of 20s after int_mset.erase(iterator): ";
-    std::cout << int_mset.count(20);
+    print_multiset(int_umset);
+    std::cout << "Number of 20s after int_umset.erase(iterator): ";
+    std::cout << int_umset.count(20);
     return 0;
 }
