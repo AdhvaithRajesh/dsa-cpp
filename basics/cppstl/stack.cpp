27bf8c1c1e662edeaba8b2e07ad400ea8309a026
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2025-07-29 14:40:47 +0530

    add use cases of stack to stack.cpp

--- basics/cppstl/stack.cpp
+++ basics/cppstl/stack.cpp
@@ -11,6 +11,17 @@ it is a container adapter - it isn't a full container, wrapper around std::deque
 restricted access - can only access the element at the top of the stack
 
 all operations are O(1)
+
+When to use stack?
+- sliding window minimum/maximum
+    - most popular use-case of deque in competitive programming
+    - find maximum/minimum element in every subarray of size k
+    - deque used to maintain list of candidate indices
+        - double ended nature helps with adding and discarding from both ends
+
+- implementing queue or stack but with iteration
+    - std::stack and std::queue are wrappers around std::deque
+    - since it is the underlying container, it can be used to implement either of these with iteration
 */
 
 template <typename T>
