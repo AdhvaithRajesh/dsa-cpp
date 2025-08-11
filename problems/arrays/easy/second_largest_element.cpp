2aba715227975e473027d680fbf67c6755296efd
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2025-08-11 13:03:20 +0530

    add second_largest_element.cpp

---
+++ problems/arrays/easy/second_largest_element.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+template <typename T>
+void print_vector(const std::vector<T> &vec)
+{
+    for (const auto &element : vec)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << "\n";
+}
+
+int secondLargestElement(std::vector<int> &da_vec)
+{
+    int second_largest_element = std::numeric_limits<int>::min();
+    int largest_element = std::numeric_limits<int>::min();
+
+    for (const auto &element : da_vec)
+    {
+        if (element > largest_element)
+        {
+            second_largest_element = largest_element;
+            largest_element = element;
+        }
+        else if (element > second_largest_element && element != largest_element)
+        {
+            second_largest_element = element;
+        }
+    }
+    if (second_largest_element == std::numeric_limits<int>::min())
+    {
+        return -1;
+    }
+    return second_largest_element;
+}
+
+int main()
+{
+    std::vector<int> nums = {12, 19, 19, 5, 4, 99, 8};
+    std::cout << "Vector: ";
+    print_vector(nums);
+    std::cout << "Second largest Element: ";
+    std::cout << secondLargestElement(nums) << "\n";
+    std::cout << secondLargestElement(nums) << "\n";
+}
